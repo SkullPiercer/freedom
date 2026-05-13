afe2ad093c66e0a844add9b169c106c9bf2ef0f8
Sasha <akatsuki_sanya@vk.com>
2026-05-13 21:37:43 +0600

    Add hashing of users passwords

--- app/services/user.py
+++ app/services/user.py
@@ -1,11 +1,26 @@
+from pwdlib import PasswordHash
+
 from app.services.base import BaseService
 from app.api.schemas.user import UserCreateRequest, UserCreateSchema
 
+class PasswordService:
+    password_hash = PasswordHash.recommended()
+
+    def verify_password(self, plain_password, hashed_password):
+        return self.password_hash.verify(plain_password, hashed_password)
+
+    def get_password_hash(self, password):
+        return self.password_hash.hash(password)
+
+
 class UserService(BaseService):
+    password_service = PasswordService()
+
     async def create_user(self, user: UserCreateRequest):
         password = user.password.get_secret_value()
         validated_data = UserCreateSchema(
-            email=user.email, hashed_password=password
+            email=user.email,
+            hashed_password=self.password_service.get_password_hash(password)
         )
         new_user = await self.db.user.create(validated_data)
 
